bdaf1638e7a0f5adf6dd64d8370730c8031470f4
ConnorVin <43933461+ConnorVin@users.noreply.github.com>
2019-02-05 12:05:22 -0800

    Update mage_character.py

--- mage_character.py
+++ mage_character.py
@@ -12,15 +12,21 @@ class MageCharacter(AbstractCharacter):
         self._attack_speed = 1
         self._spell_power = 30
         self._spell_chance = (19,20)
+
+    def get_type(self):
+        return "Mage"
     
+    def get_stats(self):
+        print("Username: {}".format(self._username))
+        print("Health: {}".format(self._health))
+        print("Attack: {}".format(self._attack))
+        print("Defence: {}".format(self._defence))
+        print("Attack Speed: {}".format(self._attack_speed))
+        print('Spell Chance: 10%')
+        print('Spell Damage: {}'.format((self._attack + self._spell_power)))
+
     def get_damage(self):
         if randint(1,20) in self._spell_chance:
             return self._attack + self._spell_power
         else:
             return super().get_damage()
-
-    def get_stats(self):
-        raise NotImplementedError
-
-    def get_type(self):
-        return "Mage"
